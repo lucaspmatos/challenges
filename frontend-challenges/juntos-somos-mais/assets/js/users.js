ecd037ebb88c7f8592ae39ac68e4c3843ac3dc0e
Marc Victor <marc.carvalho@inteligenciaweb.com.br>
2021-01-21 23:49:21 -0300

    search function implemented

--- frontend-challenges/juntos-somos-mais/assets/js/users.js
+++ frontend-challenges/juntos-somos-mais/assets/js/users.js
@@ -1,98 +1,99 @@
-let usersElement = document.querySelector("#users");
+let usersElement = document.querySelector('#users')
 
 // Função que permite o consumo da API
-(function () {
-  var cors_api_host = "cors-anywhere.herokuapp.com";
-  var cors_api_url = "https://" + cors_api_host + "/";
-  var slice = [].slice;
-  var origin = window.location.protocol + "//" + window.location.host;
-  var open = XMLHttpRequest.prototype.open;
+;(function () {
+  var cors_api_host = 'cors-anywhere.herokuapp.com'
+  var cors_api_url = 'https://' + cors_api_host + '/'
+  var slice = [].slice
+  var origin = window.location.protocol + '//' + window.location.host
+  var open = XMLHttpRequest.prototype.open
   XMLHttpRequest.prototype.open = function () {
-    var args = slice.call(arguments);
-    var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+    var args = slice.call(arguments)
+    var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1])
     if (
       targetOrigin &&
       targetOrigin[0].toLowerCase() !== origin &&
       targetOrigin[1] !== cors_api_host
     ) {
-      args[1] = cors_api_url + args[1];
+      args[1] = cors_api_url + args[1]
     }
-    return open.apply(this, args);
-  };
-})();
+    return open.apply(this, args)
+  }
+})()
 
 function capitalize(word) {
   return word
     .toLowerCase()
-    .replace(/\w/, (firstLetter) => firstLetter.toUpperCase());
+    .replace(/\w/, (firstLetter) => firstLetter.toUpperCase())
 }
 
 function treatStreet(string) {
-  const street = string.split(" ");
-  let treatedStreet = "";
+  const street = string.split(' ')
+  let treatedStreet = ''
 
-  const number = street.shift();
+  const number = street.shift()
 
   street.map((value) => {
-    if (value.length <= 2 && !value.startsWith("d")) {
-      treatedStreet += `${value}`;
-    } else if (value.length <= 3 && value.startsWith("d")) {
-      treatedStreet += ` ${value}`;
+    if (value.length <= 2 && !value.startsWith('d')) {
+      treatedStreet += `${value}`
+    } else if (value.length <= 3 && value.startsWith('d')) {
+      treatedStreet += ` ${value}`
     } else {
-      treatedStreet += ` ${capitalize(value)}`;
+      treatedStreet += ` ${capitalize(value)}`
     }
-  });
+  })
 
-  return treatedStreet + `, ${number}`;
+  return treatedStreet + `, ${number}`
 }
 
 function treatString(str) {
-  const string = str.split(" ");
-  let treatedString = "";
+  const string = str.split(' ')
+  let treatedString = ''
 
   string.map((value) => {
-    if (value.length <= 3 && value.startsWith("d")) {
-      treatedString += ` ${value}`;
+    if (value.length <= 3 && value.startsWith('d')) {
+      treatedString += ` ${value}`
     } else {
-      treatedString += ` ${capitalize(value)}`;
+      treatedString += ` ${capitalize(value)}`
     }
-  });
+  })
 
-  return treatedString;
+  return treatedString
 }
 
-let currentPage = 1;
-let recordsPerPage = 9;
-let numPages = 0;
+let currentPage = 1
+let recordsPerPage = 9
+let numPages = 0
+let users = ''
 
 async function getUsers() {
   const { data } = await axios.get(
-    "https://jsm-challenges.s3.amazonaws.com/frontend-challenge.json"
-  );
+    'https://jsm-challenges.s3.amazonaws.com/frontend-challenge.json'
+  )
 
-  return data.results;
+  return data.results
 }
 
 function prevPage() {
   if (currentPage > 1) {
-    currentPage--;
-    changePage(currentPage);
+    currentPage--
+    changePage(currentPage)
   }
 }
 
 function nextPage() {
   if (currentPage < numPages) {
-    currentPage++;
-    changePage(currentPage);
+    currentPage++
+    changePage(currentPage)
   }
 }
 
-async function getNumPages(users) {
-  numPages = Math.ceil(users.length / recordsPerPage);
+function getNumPages(usersList) {
+  numPages = Math.ceil(usersList.length / recordsPerPage)
 }
 
-let usersContainer = document.createElement("div");
-usersContainer.setAttribute("class", "users-container");
+let usersContainer = document.createElement('div')
+usersContainer.setAttribute('class', 'users-container')
 
 /* document.getElementById("search-button").addEventListener("submit", searchUser);
 
@@ -101,34 +102,41 @@ function searchUser(e) {
   const search = document.getElementById("search").value || "";
   changePage(1, search.toLowerCase());
 } */
+function searchFunction() {
+  let input = document.getElementById('search').value
+  changePage(1, input)
+}
+async function changePage(page, result = '') {
+  const test = await getUsers()
 
-async function changePage(page, result = "") {
-  const test = await getUsers();
+  const word = result
 
-  const word = result;
-  
-  const users = test.filter((value) => {
-    if (value.name.first.startsWith(word)) {
-      return value;
+  users = test.filter((value) => {
+    if (value.name.first.includes(word) || value.name.last.includes(word)) {
+      return value
     }
-  });
+  })
 
-  console.log(users);
+  console.log('users', users)
 
-  getNumPages(users);
+  getNumPages(users)
 
-  let btn_next = document.getElementById("btn_next");
-  let btn_prev = document.getElementById("btn_prev");
+  let btn_next = document.getElementById('btn_next')
+  let btn_prev = document.getElementById('btn_prev')
 
   // Validate page
-  if (page < 1) page = 1;
-  if (page > numPages) page = numPages;
+  if (page < 1) page = 1
+  if (page > numPages) page = numPages
 
-  let showUsers = "";
+  let showUsers = ''
 
-  usersContainer.innerHTML = "";
+  usersContainer.innerHTML = ''
 
-  for (let i = (page - 1) * recordsPerPage; i < (page * recordsPerPage) && i < users.length; i++) {
+  for (
+    let i = (page - 1) * recordsPerPage;
+    i < page * recordsPerPage && i < users.length;
+    i++
+  ) {
     showUsers += `
     <div id="users-box" class="users-box">
       <img src="${users[i].picture.large}" class="user-picture" />
@@ -136,29 +144,31 @@ async function changePage(page, result = "") {
         <h5>${treatString(users[i].name.first + ' ' + users[i].name.last)}</h5>
         <span class="street">${treatStreet(users[i].location.street)}</span>
         <span class="city">${treatString(users[i].location.city)} </br>
-        ${treatString(users[i].location.state)} - CEP: ${users[i].location.postcode}</span>
+        ${treatString(users[i].location.state)} - CEP: ${
+      users[i].location.postcode
+    }</span>
       </div>
     </div>
-    `;
+    `
   }
-  
-  usersContainer.innerHTML = showUsers;
 
-  usersElement.appendChild(usersContainer);
+  usersContainer.innerHTML = showUsers
+
+  usersElement.appendChild(usersContainer)
 
   if (page == 1) {
-    btn_prev.style.visibility = "hidden";
+    btn_prev.style.visibility = 'hidden'
   } else {
-    btn_prev.style.visibility = "visible";
+    btn_prev.style.visibility = 'visible'
   }
 
   if (page == numPages) {
-    btn_next.style.visibility = "hidden";
+    btn_next.style.visibility = 'hidden'
   } else {
-    btn_next.style.visibility = "visible";
+    btn_next.style.visibility = 'visible'
   }
 }
 
 window.onload = function () {
-  changePage(1, "");
-};
+  changePage(1, '')
+}
